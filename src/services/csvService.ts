fac3ca3e6f020171daad30996b44330f57701e87
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2024-12-16 00:01:41 -0400

    fix: change CSV writing method from writeFileSync to appendFileSync for user data

--- src/services/csvService.ts
+++ src/services/csvService.ts
@@ -9,11 +9,11 @@ export function writeUsersCSV(users: User[]): void {
     try {
         // Converter os usuários para string no formato CSV.
         const userString = users.map((user) => 
-            `${user.id}, ${user.name}, ${user.email}, ${user.password}, ${user.role.role}, ${user.registerDate.toISOString()}, ${user.changeDate.toISOString()}, ${user.status}`
+            `${user.id},${user.name},${user.email},${user.password},${user.role.role},${user.registerDate.toISOString()},${user.changeDate.toISOString()},${user.status}`
         )
 
         // Escrever os dados no arquivo CSV.
-        fs.writeFileSync(filePath, userString.join('\n'))
+        fs.appendFileSync(filePath, userString.join('\n'))
     } catch (err) {
         console.log(`Error: ${(err as Error).message}`)
     }
