b40934a6cad3df1ab2c53630cd1f94bbcd67ca00
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2024-12-16 10:19:22 -0400

    docs: correct comment punctuation in user seed function

--- src/seeds/usersSeeds.ts
+++ src/seeds/usersSeeds.ts
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid'
 import { registerUser } from "../services/userService"
 import { adminPermissions, guestPermissions, teachPermissions} from "./rolesSeeds"
 
-// Função para registrar os usuários seeds
+// Função para registrar os usuários seeds.
 export function seedUsers(): void {
     registerUser(uuid(), 'Admin'  , 'admin@email.com'  , '#Admin123'  , adminPermissions, true)
     registerUser(uuid(), 'Guest'  , 'guest@email.com'  , '#Guest123'  , guestPermissions, false)
