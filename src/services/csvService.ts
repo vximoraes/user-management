78e09d5164f09dd44be627d9d5f9513c90684e44
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2024-12-16 17:00:35 -0400

    refactor: improve CSV user data conversion and writing logic

--- src/services/csvService.ts
+++ src/services/csvService.ts
@@ -5,13 +5,19 @@ export const filePath: string = './data/users.csv' // Caminho para o arquivo CSV
 
 export function writeUsersCSV(users: User[]): void {
     try {
-        // Converter os usuários para string no formato CSV.
-        const userString = users.map((user) => 
-            `${user.id},${user.name},${user.email},${user.password},${user.role.role},${user.registerDate.toISOString()},${user.changeDate.toISOString()},${user.status}`
-        )
+        // Converte os dados dos usuários para uma string no formato CSV, onde cada campo é separado por vírgula.
+        const userString = users.map(user => [
+            user.id, 
+            user.name, 
+            user.email, 
+            user.password, 
+            user.role, 
+            user.registerDate.toISOString(), 
+            user.changeDate.toISOString(), 
+            user.status
+        ].join(',')).join('\n')     
 
-        // Escrever os dados no arquivo CSV.
-        fs.writeFileSync(filePath, userString.join('\n'))
+        fs.writeFileSync(filePath, userString, 'utf-8')
     } catch (err) {
         console.log(`Error: ${(err as Error).message}`) // Trata qualquer erro e exibe a mensagem de erro no console.
     }
