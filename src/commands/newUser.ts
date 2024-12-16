004200eb31aa47b333cb18fecfe9209280000dc1
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2024-12-16 19:47:49 -0400

    fix: validate user role input in newUser command

--- src/commands/newUser.ts
+++ src/commands/newUser.ts
@@ -9,10 +9,17 @@ newUser
     .argument('<name>', 'Nome do usuário a ser cadastrado')
     .argument('<email>', 'Email do usuário')
     .argument('<password>', 'Senha do usuário')
-    .argument('<role>', 'Papel do usuário (ex: admin, user, etc.)')
+    .argument('<role>', 'Papel do usuário (ex: adm, convidado, professor)')
     .argument('<status>', 'Status do usuário (true para ativo, false para inativo)')
     .action((name, email, password, role, status) => {
         try {
+            // Verifica se o papel é válido.
+            const validRoles = ['admin', 'convidado', 'professor']
+            if (!validRoles.includes(role)) {
+                console.log('\nInsira um papel válido: admin, convidado ou professor.\n')
+                return
+            }
+
             // Convertendo o status para booleano.
             const isActive = status === 'true'
             registerUser(name, email, password, role, isActive)
